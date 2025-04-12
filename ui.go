441ad23a8531a9b494a102f1e9e0e30107645a57
Justine <paralejasjustine15@gmail.com>
2025-04-13 02:08:37 +0800

    feat: file tree navigation on select files

--- ui.go
+++ ui.go
@@ -140,60 +140,109 @@ func (c *Client) sendFiles() {
 }
 
 func (c *Client) selectFiles() ([]FileInfo, error) {
-	entries, err := os.ReadDir(".")
-	if err != nil {
-		return nil, err
-	}
+	selectedFiles := make(map[string]FileInfo)
+	currentDir := "."
+	var selected string
 
-	var fileOptions []huh.Option[string]
-	for _, entry := range entries {
-		if !entry.IsDir() {
-			fileInfo, enterr := entry.Info()
-			if enterr != nil {
-				continue
-			}
-			option := fmt.Sprintf("%s (%d)", entry.Name(), fileInfo.Size())
-			fileOptions = append(fileOptions, huh.NewOption(option, entry.Name()))
-		}
-	}
-
-	if len(fileOptions) == 0 {
-		return nil, fmt.Errorf("no files found in current directory")
-	}
-
-	var selectedFileNames []string
-	form := huh.NewForm(
-		huh.NewGroup(
-			huh.NewMultiSelect[string]().
-				Title("Select files to share").
-				Options(fileOptions...).
-				Value(&selectedFileNames),
-		),
-	)
-
-	err = form.Run()
-	if err != nil {
-		return nil, err
-	}
-
-	if len(selectedFileNames) == 0 {
-		return nil, fmt.Errorf("no files selected")
-	}
-
-	var selectedFiles []FileInfo
-	for _, name := range selectedFileNames {
-		fileInfo, err := os.Stat(name)
+	for {
+		entries, err := os.ReadDir(currentDir)
 		if err != nil {
-			continue
+			return nil, err
 		}
-		selectedFiles = append(selectedFiles, FileInfo{
-			Name: name,
-			Size: fileInfo.Size(),
-			Path: filepath.Join(".", name),
-		})
-	}
 
-	return selectedFiles, nil
+		var options []huh.Option[string]
+
+		options = append(options, huh.NewOption("../", "../"))
+
+		for _, entry := range entries {
+			if entry.IsDir() {
+				options = append(options, huh.NewOption(entry.Name()+"/", entry.Name()))
+			}
+		}
+
+		for _, entry := range entries {
+			if !entry.IsDir() {
+				fileInfo, enterr := entry.Info()
+				if enterr != nil {
+					continue
+				}
+
+				name := entry.Name()
+				fullPath := filepath.Join(currentDir, name)
+				displayName := name
+
+				if _, selected := selectedFiles[fullPath]; selected {
+					displayName = SUCCESS.Render("[✓] " + name)
+				}
+
+				option := fmt.Sprintf("%s (%d bytes)", displayName, fileInfo.Size())
+				options = append(options, huh.NewOption(option, name))
+			}
+		}
+
+		options = append(options, huh.NewOption("Done", "done"))
+		options = append(options, huh.NewOption("Cancel", "cancel"))
+
+		if len(options) == 2 {
+			return nil, fmt.Errorf("no entries found in directory: %s", currentDir)
+		}
+
+		form := huh.NewForm(
+			huh.NewGroup(
+				huh.NewSelect[string]().
+					Title(fmt.Sprintf("Browsing: %s (%d files selected)", currentDir, len(selectedFiles))).
+					Options(options...).
+					Value(&selected),
+			),
+		)
+
+		err = form.Run()
+		if err != nil {
+			return nil, err
+		}
+
+		switch selected {
+		case "..":
+			currentDir = filepath.Dir(currentDir)
+			continue
+
+		case "cancel":
+			return nil, fmt.Errorf("file selection cancelled")
+
+		case "done":
+			if len(selectedFiles) == 0 {
+				return nil, fmt.Errorf("no file selected")
+			}
+
+			var result []FileInfo
+			for _, fileInfo := range selectedFiles {
+				result = append(result, fileInfo)
+			}
+			return result, nil
+
+		default:
+			fullPath := filepath.Join(currentDir, selected)
+			fileInfo, err := os.Stat(fullPath)
+			if err != nil {
+				return nil, fmt.Errorf("error accessing %s: %v", fullPath, err)
+			}
+
+			if fileInfo.IsDir() {
+				currentDir = fullPath
+			} else {
+				if _, exists := selectedFiles[fullPath]; exists {
+					delete(selectedFiles, fullPath)
+				} else {
+					selectedFiles[fullPath] = FileInfo{
+						Name: selected,
+						Size: fileInfo.Size(),
+						Path: fullPath,
+					}
+				}
+			}
+		}
+
+	}
 }
 
 func (c *Client) displayPeers() {
